0a8dd46e1ec5c249e6fb1a27725e7d7c72513348
Brad <brad@brad.tw>
2016-09-29 19:11:59 +0800

    v2

--- app/src/main/java/brad/tw/mythreadtest/MainActivity.java
+++ app/src/main/java/brad/tw/mythreadtest/MainActivity.java
@@ -14,19 +14,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test1(View v){
-        MyThread mt1 = new MyThread();
-        mt1.start();
+        MyThread mt1 = new MyThread("A");
+        MyThread mt2 = new MyThread("B");
+        mt1.start(); mt2.start();
+        //mt1.start();
 //        mt1.start();
-        mt1.run();
-        mt1.run();
-        mt1.run();
+//        mt1.run();
+//        mt1.run();
     }
 
     private class MyThread extends Thread {
+        String name;
+        MyThread(String name){this.name = name;}
         @Override
         public void run() {
             for (int i=0; i<20; i++){
-                Log.d("brad", "i = " + i);
+                Log.d("brad", name + ":" + "i = " + i);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                }
             }
 
         }
